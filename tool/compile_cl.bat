961e25f84503cd5d88c64493f79337f32c5c3fc8
phanirithvij <phanirithvij2000@gmail.com>
2019-12-07 05:15:45 +0530

    test3

--- tool/compile_cl.bat
+++ tool/compile_cl.bat
@@ -3,6 +3,7 @@ echo off
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" (
         call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+        call cl.exe
         echo "USING VISUAL STUDIO 19"
     )
 )
@@ -10,6 +11,7 @@ if NOT DEFINED VCINSTALLDIR (
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" (
         call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" amd64
+        call cl.exe
         echo "USING VISUAL STUDIO 17"
     )
 )
@@ -17,6 +19,7 @@ if NOT DEFINED VCINSTALLDIR (
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" (
         call "C:\Program Files (x86)\Microsoft Visual Studio 15.0\VC\vcvarsall.bat" amd64
+        call cl.exe
         echo "USING VISUAL STUDIO 15"
     )
 )
@@ -24,6 +27,7 @@ if NOT DEFINED VCINSTALLDIR (
 if NOT DEFINED VCINSTALLDIR (
     if exist "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
         call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" amd64
+        call cl.exe
         echo "USING VISUAL STUDIO 14"
     )
 )
